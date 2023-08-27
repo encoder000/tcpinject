5791094f5f7c798d32b3adad36cd241a8da1a474
encoder000 <94760861+encoder000@users.noreply.github.com>
2023-08-27 13:48:04 +0300

    Add files via upload
    Now you can use socks5 proxies

--- tcpinject.py
+++ tcpinject.py
@@ -3,14 +3,22 @@ import socket
 import threading
 
 class conn_:
-    def __init__(self,conn,handle_func,ignore_empty):
+    def __init__(self,conn,handle_func,ignore_empty,socks5_proxy=None):
         self.conn = conn
         self.handle_func = handle_func
         self.ignore_empty = ignore_empty
+        self.socks5_proxy = socks5_proxy
 
     def start(self,rport,rip):
         self.remote_conn = socks.socksocket()
+        
+        if self.socks5_proxy:
+            pip,pport = self.socks5_proxy.split(':')#proxy ip & proxy port
+            pport = int(pport)
+            self.remote_conn.set_proxy(socks.PROXY_TYPE_SOCKS5,pip,pport)
+            
         self.remote_conn.connect((rip,rport))
+        
         threading.Thread(target=self.srclistener).start()
         threading.Thread(target=self.remotelistener).start()
 
@@ -30,7 +38,7 @@ class conn_:
             self.handle_func(packet(data,self.conn,False,self))
 
 
-def start_deamon(srcip:str,rip:str,srcport:int,rport:int,listen:int,handle_func,ignore_empty=False):
+def start_deamon(srcip:str,rip:str,srcport:int,rport:int,listen:int,handle_func,ignore_empty=False,socks5_proxy=None):
     s = socks.socksocket()
     s.bind((srcip,srcport))
     if listen==-1:
@@ -40,16 +48,16 @@ def start_deamon(srcip:str,rip:str,srcport:int,rport:int,listen:int,handle_func,
         
     while True:
         c,a = s.accept()
-        conn = conn_(c,handle_func,ignore_empty)
+        conn = conn_(c,handle_func,ignore_empty,socks5_proxy)
         threading.Thread(target=conn.start,args=(rport,rip)).start()
 
-def start_deamon_thread(srcip:str,rip:str,srcport:int,rport:int,listen:int,handle_func,ignore_empty=False):
+def start_deamon_thread(srcip:str,rip:str,srcport:int,rport:int,listen:int,handle_func,ignore_empty=False,socks5_proxy=None):
     threading.Thread(target=start_deamon,args=(srcip,
                                                rip,
                                                srcport,
                                                rport,
                                                listen,
-                                               handle_func,ignore_empty)).start()
+                                               handle_func,ignore_empty,socks5_proxy)).start()
 
 class packet:
     def __init__(self,packet,conn,from_user:bool,conn_):
